811a3db50f25daff8d1bd5272835248f28c8c736
ibrahimakbulutt10-boop <ibrahimakbulutt10@gmail.com>
2025-10-21 18:05:18 +0300

    Update server.js

--- ritimon/server.js
+++ ritimon/server.js
@@ -108,6 +108,10 @@ app.get('/dj', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dj.html'));
 });
 
+app.get('/dj-control', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dj-control.html'));
+});
+
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
